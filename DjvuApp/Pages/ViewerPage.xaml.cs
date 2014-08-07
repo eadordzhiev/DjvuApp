c357214e1cae99839bf8cbce5e1aafcca9a55395
eadordzhiev <useless_guy@mail.ru>
2014-08-08 00:54:31 +0400

    Restored appbar in ViewerPage

--- DjvuApp/Pages/ViewerPage.xaml.cs
+++ DjvuApp/Pages/ViewerPage.xaml.cs
@@ -41,21 +41,111 @@ namespace DjvuApp
         {
             navigationHelper.OnNavigatedTo(e);
 
-            Book = (Book)e.Parameter;
-        }
+            ApplicationView.GetForCurrentView().SetDesiredBoundsMode(ApplicationViewBoundsMode.UseCoreWindow);
 
-        public Book Book { get; set; }
+            if (CurrentBook == null)
+                CurrentBook = (Book)e.Parameter;
+        }
 
         protected override void OnNavigatedFrom(NavigationEventArgs e)
         {
             navigationHelper.OnNavigatedFrom(e);
+
+            ApplicationView.GetForCurrentView().SetDesiredBoundsMode(ApplicationViewBoundsMode.UseVisible);
+
+            SaveCurrentPosition();
         }
-        
-        private async void ViewerPage_OnLoaded(object sender, RoutedEventArgs e)
+
+        public Book CurrentBook { get; set; }
+
+        public IOutlineItem Outline { get; set; }
+
+        public DjvuDocument CurrentDocument { get; set; }
+
+        private DjvuDocumentViewModel _viewModel;
+
+        private async void LoadedHandler(object sender, RoutedEventArgs e)
         {
-            var document = new DjvuDocument(Book.Path);
+            CurrentDocument = new DjvuDocument(CurrentBook.Path);
+
+            var djvuOutline = CurrentDocument.GetBookmarks();
+            if (djvuOutline != null)
+            {
+                Outline = OutlineItem.GetOutline(djvuOutline);
+                outlineButton.Visibility = Visibility.Visible;
+            }
+
             await Task.Delay(1);
-            listView.ItemsSource = new DjvuDocumentViewModel(document); 
+
+            _viewModel = new DjvuDocumentViewModel(CurrentDocument);
+            listView.ItemsSource = _viewModel;
+
+
+            GoToPage(CurrentBook.LastOpeningPageNumber);
+        }
+
+        private async void OutlineButtonClickHandler(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OutlineDialog();
+            dialog.DataContext = Outline;
+            await dialog.ShowAsync();
+
+            var pageNumber = dialog.TargetPageNumber;
+            if (pageNumber.HasValue)
+                GoToPage(pageNumber.Value);
+        }
+
+        private async void JumpToPageButtonClickHandler(object sender, RoutedEventArgs e)
+        {
+            var dialog = new JumpToPageDialog(CurrentDocument.PageCount);
+            await dialog.ShowAsync();
+
+            var pageNumber = dialog.TargetPageNumber;
+            if (pageNumber.HasValue)
+                GoToPage(pageNumber.Value);
+        }
+
+        private async Task SaveCurrentPosition()
+        {
+            var provider = ServiceLocator.Current.GetInstance<IBookProvider>();
+            //await provider.UpdateBookPositionAsync(CurrentBook, documentViewer.CurrentPageNumber);
+        }
+
+        private void GoToPage(uint pageNumber)
+        {
+            if (_viewModel == null)
+                return;
+
+            int pageIndex = (int) (pageNumber - 1);
+            var page = _viewModel[pageIndex];
+            listView.ScrollIntoView(page, ScrollIntoViewAlignment.Leading);
+        }
+
+        //private uint GetCurrentPageNumber()
+        //{
+        //}
+
+        private bool CheckIfElementIsVisible(FrameworkElement element)
+        {
+            Point point;
+            point.X = 0;
+            point.Y = 0;
+            Rect bounds = Window.Current.Bounds;
+            GeneralTransform gt = element.TransformToVisual(Window.Current.Content);
+            Point offset = gt.TransformPoint(point);
+            bool xResult = offset.X + element.ActualWidth >= 0 && offset.X < bounds.Width;
+            bool yResult = offset.Y + element.ActualHeight >= 0 && offset.Y < bounds.Height;
+            return xResult && yResult;
+        }
+
+        private void AppBar_OnOpened(object sender, object e)
+        {
+            StatusBar.GetForCurrentView().ShowAsync();
+        }
+
+        private void AppBar_OnClosed(object sender, object e)
+        {
+            StatusBar.GetForCurrentView().HideAsync();
         }
     }
 }
