7f7ba624123ab1fd9b00bcd916b38557057ea3b3
eadordzhiev <useless_guy@mail.ru>
2014-08-07 22:51:29 +0400

    Initial listview commit

--- DjvuApp/Pages/ViewerPage.xaml.cs
+++ DjvuApp/Pages/ViewerPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.DataTransfer;
 using Windows.Storage;
@@ -20,6 +21,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using DjvuApp.ViewModel;
 using DjvuLibRT;
 using Microsoft.Practices.ServiceLocation;
 
@@ -28,99 +30,32 @@ namespace DjvuApp
     public sealed partial class ViewerPage : Page
     {
         private NavigationHelper navigationHelper;
-        private bool isAppBarVisible = true;
-
         public ViewerPage()
         {
             this.InitializeComponent();
             
             this.navigationHelper = new NavigationHelper(this);
-            //this.navigationHelper.LoadState += this.NavigationHelper_LoadState;
-            //this.navigationHelper.SaveState += this.NavigationHelper_SaveState;
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            
-
             navigationHelper.OnNavigatedTo(e);
 
-            if (CurrentBook != null)
-                return;
-
-            CurrentBook = (Book)e.Parameter;
+            Book = (Book)e.Parameter;
         }
 
-        protected override async void OnNavigatedFrom(NavigationEventArgs e)
-        {
-            ApplicationView.GetForCurrentView().SetDesiredBoundsMode(ApplicationViewBoundsMode.UseVisible);
+        public Book Book { get; set; }
 
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
             navigationHelper.OnNavigatedFrom(e);
-
-            SaveCurrentPosition();
-        }
-
-        public Book CurrentBook { get; set; }
-
-        public IOutlineItem Outline { get; set; }
-
-        public DjvuDocument CurrentDocument { get; set; }
-
-        private async void LoadedHandler(object sender, RoutedEventArgs e)
-        {
-            ApplicationView.GetForCurrentView().SetDesiredBoundsMode(ApplicationViewBoundsMode.UseCoreWindow);
-
-            CurrentDocument = new DjvuDocument(CurrentBook.Path);
-
-            var djvuOutline = CurrentDocument.GetBookmarks();
-            if (djvuOutline != null)
-            {
-                Outline = OutlineItem.GetOutline(djvuOutline);
-                outlineButton.Visibility = Visibility.Visible;
-            }
-
-            documentViewer.Source = CurrentDocument;
-
-            await Task.Delay(1);
-
-            documentViewer.CurrentPageNumber = CurrentBook.LastOpeningPageNumber;
-        }
-
-        private async void OutlineButtonClickHandler(object sender, RoutedEventArgs e)
-        {
-            var dialog = new OutlineDialog();
-            dialog.DataContext = Outline;
-            await dialog.ShowAsync();
-
-            var pageNumber = dialog.TargetPageNumber;
-            if (pageNumber.HasValue)
-                documentViewer.CurrentPageNumber = pageNumber.Value;
-        }
-
-        private async void JumpToPageButtonClickHandler(object sender, RoutedEventArgs e)
-        {
-            var dialog = new JumpToPageDialog(CurrentDocument.PageCount);
-            await dialog.ShowAsync();
-
-            var pageNumber = dialog.TargetPageNumber;
-            if (pageNumber.HasValue)
-                documentViewer.CurrentPageNumber = pageNumber.Value;
-        }
-
-        private async Task SaveCurrentPosition()
-        {
-            var provider = ServiceLocator.Current.GetInstance<IBookProvider>();
-            await provider.UpdateBookPositionAsync(CurrentBook, documentViewer.CurrentPageNumber);
         }
         
-        private void AppBar_OnOpened(object sender, object e)
+        private async void ViewerPage_OnLoaded(object sender, RoutedEventArgs e)
         {
-            StatusBar.GetForCurrentView().ShowAsync();
-        }
-
-        private void AppBar_OnClosed(object sender, object e)
-        {
-            StatusBar.GetForCurrentView().HideAsync();
+            var document = new DjvuDocument(Book.Path);
+            await Task.Delay(1);
+            listView.ItemsSource = new DjvuDocumentViewModel(document); 
         }
     }
 }
---
+++ DjvuApp/ViewModel/DjvuDocumentViewModel.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using DjvuLibRT;
+using GalaSoft.MvvmLight.Threading;
+
+namespace DjvuApp.ViewModel
+{
+    public sealed class DjvuDocumentViewModel : ObservableCollection<DjvuPageViewModel>
+    {
+        private readonly DjvuDocument _document;
+
+        public DjvuDocumentViewModel(DjvuDocument document, Size? size = null)
+        {
+            _document = document;
+
+            Stopwatch s = Stopwatch.StartNew();
+                var pageInfos = _document.GetPageInfos();
+                for (uint i = 0; i < _document.PageCount; i++)
+                {
+                    if (size != null)
+                    {
+                        pageInfos[i].Width = (uint) size.Value.Width;
+                        pageInfos[i].Height = (uint) size.Value.Height;
+                    }
+                    Add(new DjvuPageViewModel(_document, i + 1, pageInfos[i]));
+                }
+            s.Stop();
+        }
+    }
+
+    public sealed class DjvuPageViewModel
+    {
+        public uint PageNumber { get; set; }
+        private readonly DjvuDocument _document;
+        public double Width { get; set; }
+
+        public double Height { get; set; }
+
+        public ImageSource Source
+        {
+            get { return RenderImpl(); }
+        }
+
+        private DjvuPage _djvuPage;
+        private double _currentScale = 1 / 10D;
+
+        public DjvuPageViewModel(DjvuDocument document, uint pageNumber, PageInfo pageInfo)
+        {
+            PageNumber = pageNumber;
+            _document = document;
+
+            Width = pageInfo.Width;
+            Height = pageInfo.Height;
+        }
+
+        private ImageSource RenderImpl()
+        {
+            var s = Stopwatch.StartNew();
+
+            if (_djvuPage == null)
+                _djvuPage = _document.GetPage(PageNumber);
+
+            var pixelWidth = (int)(Width * _currentScale);
+            var pixelHeight = (int)(Height * _currentScale);
+            var size = new Size(pixelWidth, pixelHeight);
+
+            var source = new WriteableBitmap(pixelWidth, pixelHeight);
+
+            _djvuPage.RenderRegion(source, size, new Rect(new Point(), size));
+
+            s.Stop();
+            Debug.WriteLine("RenderImpl(): page {1}, {0} ms taken", s.ElapsedMilliseconds, PageNumber);
+
+            return source;
+        }
+    }
+}
