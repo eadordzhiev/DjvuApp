541d435e1ff874140ccfb4a7913b08270fb54e95
eadordzhiev <useless_guy@mail.ru>
2014-08-13 21:58:52 +0400

    Added missing file to the source control, fixed gitignore and enabled NuGet Package Restore

---
+++ DjvuApp/Model/Books/IBookProvider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace DjvuApp.Model.Books
+{
+    public interface IBookProvider
+    {
+        Task<IList<IBook>> GetBooksAsync();
+        Task<IBook> AddBookAsync(IStorageFile file);
+        Task RemoveBookAsync(IBook book);
+        Task ChangeTitleAsync(IBook book, string title);
+    }
+}
