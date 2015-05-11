411c8a7ac93aa6e498449335ac03a2fce29442a7
eadordzhiev <useless_guy@mail.ru>
2015-05-11 19:11:18 +0300

    Fixed starting crashing after downloading from the Store. A slight code reorganization.

--- DjvuApp.WindowsPhone/Properties/AssemblyInfo.cs
+++ DjvuApp.WindowsPhone/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DjvuApp")]
+[assembly: AssemblyTitle("DjvuApp.WindowsPhone")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DjvuApp")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("DjvuApp.WindowsPhone")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
