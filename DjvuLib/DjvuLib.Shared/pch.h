03cc08fbf26893a7b13b24192b780e1b1415f816
Eldar Dordzhiev <useless_guy@live.com>
2016-03-08 15:58:15 +0300

    Remove unneeded includes in pch.h

--- DjvuLib/DjvuLib.Shared/pch.h
+++ DjvuLib/DjvuLib.Shared/pch.h
@@ -1,18 +1,9 @@
-﻿//
-// pch.h
-// Header for standard system include files.
-//
+﻿#pragma once
 
-#pragma once
-
-#include <cctype>
 #include <cassert>
 #include <string>
 #include <sstream>
 #include <windows.h>
 #include <collection.h>
 #include <ppltasks.h>
-#include <windows.ui.xaml.media.dxinterop.h>
-#include <d2d1helper.h>
-#include <wrl.h>
 #include <robuffer.h>
